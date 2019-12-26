d19160e78cac006c2b480dd82110dfb6b70e9b66
Gleison de souza luiz <gleisonnanet@gmail.com>
2019-12-26 16:17:17 -0300

    first commit

---
+++ tests/CpfGratisTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace gleisonnanet\CpfGratis;
+
+use PHPUnit_Framework_TestCase;
+
+class CpfGratisTest extends PHPUnit_Framework_TestCase {
+
+    public function testGetParams() {
+
+        $this->assertEquals(true, true);
+    }
+
+}
